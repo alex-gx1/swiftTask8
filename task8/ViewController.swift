03478c7f0343b0241aac1221883658c84b98d9ef
Алексей Кононенко <alekseikononenko4@gmail.com>
2024-02-02 19:44:08 +0300

    start main

--- task8/ViewController.swift
+++ task8/ViewController.swift
@@ -1,24 +1,12 @@
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController {
 
     @IBOutlet private weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        tableView.dataSource = self
+  
     }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "Cell \(indexPath.row)"
-        return cell
-    }
-
 }
 
