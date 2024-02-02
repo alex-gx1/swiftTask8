a2cef25ac214ae494256f3c5dfbfff825d2fb811
Алексей Кононенко <alekseikononenko4@gmail.com>
2024-02-02 19:51:29 +0300

    list

--- task8/ViewController.swift
+++ task8/ViewController.swift
@@ -1,12 +1,36 @@
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+    
+    var names: [String] = ["Alfred", "Alexandr", "Ivan"]
 
     @IBOutlet private weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
   
     }
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var configuration = UIListContentConfiguration.cell()
+        configuration.text = names[indexPath.row]
+        cell.contentConfiguration = configuration
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
 
